69f8f9b7142e87fa59e37dea26636817e6033129
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-11-18 13:15:53 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,6 +1,9 @@
 import math
 from collections import Counter
+
 import pandas
+import matplotlib.pyplot as plt
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import model_selection
 from sklearn.metrics import accuracy_score
@@ -9,15 +12,17 @@ class KNNClassifier():
     def __init__(self, n_neighbors=5):
         self.K = n_neighbors
     
-    def fit(self, data, labels):
+    def fit(self, data, labels, p=2):
         self.X = data
         self.labels = labels
+        self.p=p
     
     def euclideanDistance(self, vec1, vec2):
+        p = self.p
         dist = 0
         for i in range(len(vec1)):
-            dist += (vec2[i]-vec1[i])**2
-        return math.sqrt(dist)
+            dist += (vec2[i]-vec1[i])**p
+        return dist**(1/p)
     
     def nearestPointLabels(self, testVec):
         distances=list()
@@ -47,23 +52,30 @@ class KNNClassifier():
             y_pred.append(majorLabel)
         return y_pred
             
-iris = pandas.read_csv("iris.csv")
+iris = pandas.read_csv("Data/iris.csv")    
 iris=iris.drop("species", axis=1).to_numpy()
 iris_L = [0]*50+[1]*50+[2]*50
 
 X_train,X_test,y_train,y_test=model_selection.train_test_split(iris,iris_L,test_size=0.1)
 
-model1 = KNNClassifier(n_neighbors=5)
-model1.fit(X_train, y_train)
-y_pred = model1.predict(X_test)
-print(accuracy_score(y_test,y_pred))
-print(y_pred)
-print(y_test)
-print()
+k_values = list(range(1,50,2))
+accModel1= []
+accModel2= []
 
-model2 = KNeighborsClassifier(n_neighbors=5)
-model2.fit(X_train, y_train)
-y_pred = list(model2.predict(X_test))
-print(accuracy_score(y_test,y_pred))
-print(y_pred)
-print(y_test)
+for k in k_values:
+    model1 = KNNClassifier(n_neighbors=k)
+    model1.fit(X_train, y_train)
+    y_pred = model1.predict(X_test)
+    accModel1.append(accuracy_score(y_test,y_pred)*100)
+    
+for k in k_values:
+    model2 = KNeighborsClassifier(n_neighbors=k)
+    model2.fit(X_train, y_train)
+    y_pred = model2.predict(X_test)
+    accModel2.append(accuracy_score(y_test,y_pred)*100)
+
+plt.plot(accModel1, accModel2)
+plt.title("KNN Classifier Accuracy comparison")
+plt.xlabel("accuracy of our knn")
+plt.ylabel("accuracy of sklearn's knn")
+plt.show()
