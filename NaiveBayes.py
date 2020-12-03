82144b74f8afbf8071d0214c6813b706b82c18dc
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-12-03 14:21:00 +0530

    Add files via upload

---
+++ NaiveBayes.py
@@ -0,0 +1,7 @@
+import pandas
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier,KNeighborsRegressor
+from sklearn import model_selection
+from sklearn.metrics import accuracy_score, r2_score
+
