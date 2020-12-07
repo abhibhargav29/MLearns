3777812f4c0900c203d6ee8f2f079fc8aae0827e
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-12-07 13:11:14 +0530

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas
 import matplotlib.pyplot as plt
 import math
 import numpy as np
@@ -8,51 +8,71 @@ from sklearn import model_selection
 from sklearn.metrics import accuracy_score
 
 class NaiveBayes():
-    def __init__(self, alpha=0.1):
-        self.alpha = alpha
-        
-    def separate_by_classes(self, X, y):
-        self.classes = np.unique(y)
-        classes_index = {}
-        subdatasets = {}
-        cls, counts = np.unique(y, return_counts=True)
-        self.class_freq = dict(zip(cls, counts))
-        print(self.class_freq)
-        for class_type in self.classes:
-            classes_index[class_type] = np.argwhere(y==class_type)
-            subdatasets[class_type] = X[classes_index[class_type], :]
-            self.class_freq[class_type] = self.class_freq[class_type]/sum(list(self.class_freq.values()))
-        return subdatasets
+    def __init__(self):
+        pass
+
+    def separateByClass(self, X, y):
+        classes, counts = np.unique(y, return_counts=True)
+        self.classes = classes
+        self.class_counts = dict(zip(classes, counts))
+        self.class_freq = {}
+        datasets = {}
+
+        for c in self.classes:
+            datasets[c] = X[np.argwhere(y==c), :]
+            self.class_freq[c] = self.class_counts[c]/sum(list(self.class_counts.values()))
+        return datasets
 
     def fit(self, X, y):
-        separated_X = self.separate_by_classes(X, y)
+        datasets = self.separateByClass(X, y)
         self.means = {}
         self.std = {}
-        for class_type in self.classes:
-            self.means[class_type] = np.mean(separated_X[class_type], axis=0)[0]
-            self.std[class_type] = np.std(separated_X[class_type], axis=0)[0]
+        for c in self.classes:
+            self.means[c] = np.mean(datasets[c], axis=0)[0]
+            self.std[c] = np.std(datasets[c], axis=0)[0]
+    
+    def calculateProb(self, x, mean, std):
+        exponent = math.exp(-((x-mean)**2)/(2*(std**2)))
+        return (1/math.sqrt(2*math.pi)*std)*exponent
 
-    def calculate_probability(self, x, mean, stdev):
-        exponent = math.exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
-        return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
+    def predictProb(self, X):
+        classProb = {c:math.log(self.class_freq[c], math.e) for c in self.classes}
+        for c in self.classes:
+            temp = classProb[c]
+            for i in range(len(X)):
+                temp += math.log(self.calculateProb(X[i], self.means[c][i], self.std[c][i]), math.e)
+            classProb[c] = math.exp(temp)
+        return classProb
 
     def predict(self, X):
-        pred = []
+        y_pred = []
         for x in X:
             pred_class = None
             max_prob = 0
-            for cls, prob in self.predict_proba(x).items():
+            for c, prob in self.predictProb(x).items():
                 if prob>max_prob:
                     max_prob = prob
-                    pred_class = cls
-            pred.append(pred_class)
-        return pred
+                    pred_class = c
+            y_pred.append(pred_class)
+        return y_pred
 
-    def predict_proba(self, X):
-        self.class_prob = {cls:math.log(self.class_freq[cls], math.e) for cls in self.classes}
-        for cls in self.classes:
-            for i in range(len(self.means)):
-                print(X[i])
-                self.class_prob[cls]+=math.log(self.calculate_probability(X[i], self.means[cls][i], self.std[cls][i]), math.e)
-        self.class_prob = {cls: math.e**self.class_prob[cls] for cls in self.class_prob}
-        return self.class_prob
+if __name__=="__main__":
+    #Load iris data and split
+    iris = pandas.read_csv("Data/iris.csv")    
+    iris=iris.drop("species", axis=1).to_numpy()
+    iris_L = [0]*50+[1]*50+[2]*50
+    X_train,X_test,y_train,y_test=model_selection.train_test_split(iris,iris_L,test_size=0.1)
+
+    #Predict on iris data
+    model1 = NaiveBayes()
+    model1.fit(X_train, y_train)
+    y_pred = model1.predict(X_test)
+    accModel1 = round(accuracy_score(y_test, y_pred)*100,2)
+
+    model2 = GaussianNB(var_smoothing=0)
+    model2.fit(X_train, y_train)
+    y_pred = model2.predict(X_test)
+    accModel2 = round(accuracy_score(y_test, y_pred)*100,2)
+
+    print(accModel1)
+    print(accModel2)
