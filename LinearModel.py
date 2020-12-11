b7a21eed6f011ed8539c9a5bb642743ce27f39f4
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-12-11 13:06:36 +0530

    Update LinearModel.py

--- LinearModel.py
+++ LinearModel.py
@@ -39,37 +39,24 @@ class LogReg:
 
 
 class LinReg() : 
-    def __init__(self, learning_rate=0.05, iterations=500) : 
+    def __init__(self, learning_rate=0.05, iterations=100): 
         self.learning_rate = learning_rate 
-        self.iterations = iterations 
+        self.num_iter = iterations 
               
-    def fit( self, X, Y ) :  
+    def fit(self, X, Y):  
         self.X = X 
         self.Y = Y
+        self.m, self.n = X.shape
 
-        self.m, self.n = X.shape 
+        #Gradient Descent
         self.W = np.zeros(self.n)
         self.b = 0  
-
         i=0    
-        while(i<self.iterations):   
-            self.update_weights() 
+        while(i<self.num_iter):   
+            Y_pred = X.dot(self.W) + self.b
+            self.W = self.W - self.learning_rate * (-(2*(self.X.T).dot(self.Y - Y_pred))/self.m) 
+            self.b = self.b - self.learning_rate * (-2*np.sum(self.Y - Y_pred)/self.m)  
             i+=1
-              
-        return self
-        
-    def update_weights( self ) : 
-        Y_pred = self.predict( self.X ) 
-          
-        # calculate gradients   
-        dW = - ( 2 * ( self.X.T ).dot( self.Y - Y_pred )  ) / self.m
-        db = - 2 * np.sum( self.Y - Y_pred ) / self.m  
-          
-        # update weights 
-        self.W = self.W - self.learning_rate * dW 
-        self.b = self.b - self.learning_rate * db 
-          
-        return self
       
     def predict(self, X) : 
         return X.dot(self.W) + self.b
@@ -100,7 +87,7 @@ if __name__=="__main__":
 
     print("Our Accuracies:", accModel1)
     print("Sklearn's Accuracies", accModel2)
-    plt.plot(accModel1, accModel2)
+    plt.scatter(accModel1, accModel2)
     plt.title("Logistic Regression Classifier Accuracy comparison")
     plt.xlabel("accuracy of our log reg")
     plt.ylabel("accuracy of sklearn's log reg")
